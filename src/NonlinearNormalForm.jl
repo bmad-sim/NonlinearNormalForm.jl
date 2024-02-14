3a1aaa46f0f57ebf0bb125f9efe37901cdfbdfda
Matt Signorelli <mgs255@cornell.edu>
2024-02-13 20:14:05 -0500

    Compiles, added GTPSA dependency

--- src/NonlinearNormalForm.jl
+++ src/NonlinearNormalForm.jl
@@ -2,68 +2,69 @@ module NonlinearNormalForm
 
 import Base: getindex,
              setindex!,
-             convert
+             convert,
+             ∘,
+             promote_rule
 
 using GTPSA
 
 export ComplexDAMap, DAMap
 
-struct DAMap
-  ref::Vector{Float64}
-  v::Vector{TPS}
+struct DAMap{T<:Union{TPS,ComplexTPS}}
+  v::Vector{T}
 end
 
-struct ComplexDAMap
-  ref::Vector{ComplexF64}
-  v::Vector{ComplexTPS}
+const ComplexDAMap = DAMap{ComplexTPS}
+
+promote_rule(::Type{DAMap{TPS}}, ::Type{DAMap{TPS}}) = DAMap{TPS}
+# promote_rule(::Type{ComplexTPS}, ::Union{Type{Complex},Type{AbstractFloat}, Type{Integer}, Type{Rational}, Type{AbstractIrrational}}) = ComplexTPS
+# promote_rule(::Type{TPS}, ::Union{Type{ComplexTPS}, Type{Complex}}) = ComplexTPS
+
+#= Simple helper functions
+get_cdesc(t::Union{TPS,ComplexTPS}) = unsafe_load(Base.unsafe_convert(Ptr{GTPSA.Desc}, unsafe_load(t.tpsa).d))
+get_cdesc(m::Vector{<:Union{TPS,ComplexTPS}}) = get_cdesc(first(m))
+get_cdesc(da::DAMap) = unsafe_load(first(da.v).desc)
+=#
+
+function DAMap(da::Union{DAMap,Vector{<:Union{TPS,ComplexTPS,Number}},Nothing}; use::Union{Descriptor,TPS,ComplexTPS,Nothing}=nothing)
+  low_DAMap(da, use)
 end
 
-convert(DAMap, m::Vector{TPS}) = DAMap(m)
-convert(ComplexDAMap, m::Vector{ComplexTPS}) = ComplexDAMap(m)
-
-function DAMap(d::Descriptor=GTPSA.desc_current)
-  v = vars(d)
-  nv = length(v)
-  ref = zeros(Float64, nv)
-  return DAMap(ref, v)
+# Use latest descriptor
+function low_DAMap(da::Nothing, use::Nothing)
+  return DAMap(vars(GTPSA.desc_current))
 end
 
-function DAMap(m::Vector{TPS})
-  nv = GTPSA.get_cdesc(first(m)).nv
+# Copy ctor: use DAMap Descriptor
+function low_DAMap(da::DAMap, use::Nothing)
+  return DAMap(deepcopy(da.v))
+end
+
+# Use descriptor in TPSA map
+function low_DAMap(m::Union{Vector{<:Union{TPS,ComplexTPS}}}, use::Nothing)
+  nv = unsafe_load(Base.unsafe_convert(Ptr{GTPSA.Desc}, unsafe_load(t.tpsa).d)).nv
   length(m) == nv || error("Length of $(typeof(m)) != Number of variables")
-  ref = Vector{Float64}(undef, nv)
-  da = DAMap(ref, m)
-  rezero!(da)
-  return da
+  return DAMap(deepcopy(m))
 end
 
-function ComplexDAMap(d::Descriptor=GTPSA.desc_current)
-  v = complexvars(d)
-  nv = length(v)
-  ref = zeros(ComplexF64, nv)
-  return ComplexDAMap(ref, v)
-end
-
-function ComplexDAMap(m::Vector{ComplexTPS})
-  nv = GTPSA.get_cdesc(first(m)).nv
-  length(m) == nv || error("Length of $(typeof(m)) != Number of variables")
-  ref = Vector{ComplexF64}(undef, nv)
-  da = ComplexDAMap(ref, m)
-  rezero!(da)
-  return da
-end
-
-# Indexing should be like TPSA
+# Indexing should be like TPSA map, note will return TPSA
 getindex(m::Union{DAMap,ComplexDAMap}, i::Integer) = m.v[i]
 setindex!(m::Union{DAMap,ComplexDAMap}, t::Number, i::Integer) = setindex!(m.v, t, i)
 
-# For all operations, we need something to get the zeroth order part after and put into ref
-function rezero!(da::Union{DAMap,ComplexDAMap})
-  for i in LinearIndices(da.v)
-    @inbounds da.ref[i] = da.v[i][0]
-    @inbounds da.v[i][0] = 0
+
+# Only difference is in map composition
+function ∘(d1::Union{DAMap,ComplexDAMap},d2::Union{DAMap,ComplexDAMap})
+  ref = Vector{eltype(d2.v)}(undef, length(d2.v))
+  for i=1:length(d2.v)
+    ref[i] = d2.v[i][0]
+    d2.v[i][0] = 0
   end
+  tmp = ∘(d1.v, d2.v)
+  
+  
 end
+
+
 
 #=
 # Operators:
