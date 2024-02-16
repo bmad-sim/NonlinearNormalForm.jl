e9429811fc2668f945c0711336f8dc51e6f27385
Matt Signorelli <mgs255@cornell.edu>
2024-02-16 16:58:37 -0500

    GTPSA and DA Maps, show nice

--- src/NonlinearNormalForm.jl
+++ src/NonlinearNormalForm.jl
@@ -1,87 +1,113 @@
 module NonlinearNormalForm
 
+using Reexport
+using GTPSA
+
+@reexport using GTPSA
+
 import Base: getindex,
              setindex!,
              convert,
              ∘,
-             promote_rule
+             one,
+             promote_rule,
+             show
 
-using GTPSA
+export ComplexDAMap, DAMap, GTPSAMap, ComplexGTPSAMap, TaylorMap, Descriptor
 
-export ComplexDAMap, DAMap
+abstract type TaylorMap{S<:Union{Float64,ComplexF64}, T<:Union{TPS,ComplexTPS}} end
 
-struct DAMap{T<:Union{TPS,ComplexTPS}}
+struct DAMap{S,T} <: TaylorMap{S,T}
+  x0::Vector{S}   # ENTRANCE VALUE OF THE MAP!
+  v::Vector{T}    # Expansion around x0, with scalar part equal to EXIT value of map
+end
+
+struct GTPSAMap{S,T} <: TaylorMap{S,T}
+  x0::Vector{S} 
   v::Vector{T}
 end
 
-const ComplexDAMap = DAMap{ComplexTPS}
+#const ComplexGTPSAMap = GTPSAMap{ComplexF64, ComplexTPS}
+#const ComplexDAMap = DAMap{ComplexF64, ComplexTPS}
 
-promote_rule(::Type{DAMap{TPS}}, ::Type{DAMap{TPS}}) = DAMap{TPS}
-# promote_rule(::Type{ComplexTPS}, ::Union{Type{Complex},Type{AbstractFloat}, Type{Integer}, Type{Rational}, Type{AbstractIrrational}}) = ComplexTPS
-# promote_rule(::Type{TPS}, ::Union{Type{ComplexTPS}, Type{Complex}}) = ComplexTPS
+#promote_rule(t1::Type{DAMap{<:Real,TPS}}, t2::Type{DAMap{<:Real,TPS}}) = t1{Float64,TPS}
+#promote_rule(t1::Type{DAMap{<:Number,Union{TPS,ComplexTPS}}},t2::Type{DAMap{<:Number,Union{TPS,ComplexTPS}}}) = t1{ComplexF64,ComplexTPS}
 
-#= Simple helper functions
-get_cdesc(t::Union{TPS,ComplexTPS}) = unsafe_load(Base.unsafe_convert(Ptr{GTPSA.Desc}, unsafe_load(t.tpsa).d))
-get_cdesc(m::Vector{<:Union{TPS,ComplexTPS}}) = get_cdesc(first(m))
-get_cdesc(da::DAMap) = unsafe_load(first(da.v).desc)
-=#
+#promote_rule(t1::Type{GTPSAMap{<:Real,TPS}}, t2::Type{GTPSAMap{<:Real,TPS}}) = t1{Float64,TPS}
+#promote_rule(t1::Type{GTPSAMap{<:Number,Union{TPS,ComplexTPS}}},t2::Type{GTPSAMap{<:Number,Union{TPS,ComplexTPS}}}) = t1{ComplexF64,ComplexTPS}
 
-function DAMap(da::Union{DAMap,Vector{<:Union{TPS,ComplexTPS,Number}},Nothing}; use::Union{Descriptor,TPS,ComplexTPS,Nothing}=nothing)
-  low_DAMap(da, use)
+# Disallow just adding scalars for now, maybe forever:
+# promote_rule(::Type{DAMap{TPS}}, ::Type{Vector{<:Union{Type{AbstractFloat}, Type{Integer}, Type{Rational}, Type{AbstractIrrational}}}}) = DAMap{TPS}
+# promote_rule(::Type{DAMap{ComplexTPS}}, ::Type{Vector{<:Union{Type{Complex},Type{AbstractFloat}, Type{Integer}, Type{Rational}, Type{AbstractIrrational}}}}) = DAMap{ComplexTPS}
+
+function DAMap(m::Union{TaylorMap,Nothing}=nothing; use::Union{Descriptor,TaylorMap,Nothing}=nothing)
+  return low_map(DAMap, m, use)
+end
+
+function GTPSAMap(m::Union{TaylorMap,Nothing}=nothing; use::Union{Descriptor,TaylorMap,Nothing}=nothing)
+  return low_map(GTPSAMap, m, use)
+end
+
+# Create identity map using specified Descriptor
+function low_map(type::Type, m::Nothing, use::Descriptor)
+  desc = unsafe_load(use.desc)
+  nv = desc.nv
+  np = desc.np
+  x0 = zeros(nv) 
+  v = Vector{TPS}(undef, nv+np)
+  for i=1:nv+np
+    t = TPS(use=use)
+    GTPSA.mad_tpsa_seti!(t.tpsa, Cint(i), 0.0, 1.0)
+    v[i] = t
+  end
+  return (type)(x0, v)
 end
 
 # Use latest descriptor
-function low_DAMap(da::Nothing, use::Nothing)
-  return DAMap(vars(GTPSA.desc_current))
+function low_map(type::Type, m::Nothing, use::Nothing)
+  return low_map(type, m, GTPSA.desc_current)
 end
 
-# Copy ctor: use DAMap Descriptor
-function low_DAMap(da::DAMap, use::Nothing)
-  return DAMap(deepcopy(da.v))
+# Copy ctor: use DAMap/GTPSA map Descriptor
+function low_map(type::Type, m::TaylorMap, use::Nothing)
+  return (type)(deepcopy(m.x0), deepcopy(m.v))
 end
 
-# Use descriptor in TPSA map
-function low_DAMap(m::Union{Vector{<:Union{TPS,ComplexTPS}}}, use::Nothing)
-  nv = unsafe_load(Base.unsafe_convert(Ptr{GTPSA.Desc}, unsafe_load(t.tpsa).d)).nv
+function low_map(type::Type, m::TaylorMap, use::Union{Descriptor,TaylorMap})
+  error("Changing descriptor of maps not yet implemented.")
+end
+
+# Use descriptor in Vector of TPSA. This probably shouldn't be done.
+# Do we need a probe?
+#= NOT ALLOWED!
+function low_map(type::Type, m::Vector{<:Union{TPS,ComplexTPS}}, use::Nothing)
+  nv = unsafe_load(Base.unsafe_convert(Ptr{GTPSA.Desc}, unsafe_load(first(m).tpsa).d)).nv
   length(m) == nv || error("Length of $(typeof(m)) != Number of variables")
-  return DAMap(deepcopy(m))
+  return (type)(zeros(GTPSA.numtype(first(m)), nv), deepcopy(m))
 end
+=#
 
-# Indexing should be like TPSA map, note will return TPSA
-getindex(m::Union{DAMap,ComplexDAMap}, i::Integer) = m.v[i]
-setindex!(m::Union{DAMap,ComplexDAMap}, t::Number, i::Integer) = setindex!(m.v, t, i)
+getdesc(m::TaylorMap) = unsafe_load(Base.unsafe_convert(Ptr{GTPSA.Desc}, unsafe_load(first(m.v).tpsa).d))
+numvars(m::TaylorMap) = unsafe_load(Base.unsafe_convert(Ptr{GTPSA.Desc}, unsafe_load(first(m.v).tpsa).d)).nv
+numparams(m::TaylorMap) = unsafe_load(Base.unsafe_convert(Ptr{GTPSA.Desc}, unsafe_load(first(m.v).tpsa).d)).np
+
 
 
 # Only difference is in map composition
-function ∘(d1::Union{DAMap,ComplexDAMap},d2::Union{DAMap,ComplexDAMap})
+function ∘(d1::DAMap,d2::DAMap)
   ref = Vector{eltype(d2.v)}(undef, length(d2.v))
   for i=1:length(d2.v)
     ref[i] = d2.v[i][0]
     d2.v[i][0] = 0
   end
-  tmp = ∘(d1.v, d2.v)
-  
-  
+  out = ∘(d1.v, d2.v)
+  for i=1:length(d2.v)
+    d2.v[i][0] = ref[i]
+  end
+  return out
 end
 
-
-
-#=
-# Operators:
-for op = (:+, :-, :*, :/, :^)   #, :± :∓ , :⨰ , :⨱, :⤊ 
-  eval(quote
-    Base.$op(d1::Union{DAMap,ComplexDAMap},d2::Union{DAMap,ComplexDAMap}) = GTPSA.$op(d1.v, d2.v)
-    Base.$op(d1::Union{DAMap,ComplexDAMap}, a::Number) = GTPSA.$op(d1.v, a)
-    Base.$op(a::Number, d1::Union{DAMap,ComplexDAMap}) = GTPSA.$op(a, d1.v)
-  end)
-end
-Base.∘(d1::Union{DAMap,ComplexDAMap},d2::Union{DAMap,ComplexDAMap}) = ∘(d1.v, d2.v)
-for un = (:abs, :sqrt, :exp, :log, :sin, :cos, :tan, :csc, :sec, :cot, :sinc, :sinh, :cosh, 
-          :tanh, :csch, :sech, :coth, :asin, :acos, :atan, :acsc, :asec, :acot, :asinh, :acosh, 
-          :atanh, :acsch, :asech, :acoth)
-
-+(d1::DAMap, d2::ComplexDAMap) = +(d1.v, d2.v)
-=#  
+include("show.jl")
 
 
 
---
+++ src/show.jl
@@ -0,0 +1,13 @@
+function show(io::IO, m::TaylorMap)
+  println(io, typeof(m),":")
+  extralines=0
+  if GTPSA.show_header
+    println(io, "-----------------------")
+    desc = unsafe_load(Base.unsafe_convert(Ptr{Desc}, unsafe_load(first(m.v).tpsa).d))
+    show_GTPSA_info(io, desc)
+    println(io, "-----------------------")
+    extralines = 2 + (desc.nv > 0 ? 2 : 0) + (desc.np > 0 ? 2 : 0)
+  end
+  println(io, "x0 = ", m.x0)
+  GTPSA.show_map(io, m.v, 1+extralines, true)
+end
