65846ac39e14b1e9c97943abae1f090660c20079
mattsignorelli <mgs255@cornell.edu>
2026-06-01 10:20:38 -0400

    clean

--- src/bengtsson.jl
+++ src/bengtsson.jl
@@ -3,7 +3,6 @@ function compute_bengtsson(a0, a1, m)
   # and value of type complex scalar if np == 0 or complex TPS if np > 0.
   # This is type stable bc this is stored in the type.
   nhv = nhvars(m)
-  nv = nvars(m)
   np = nparams(m)
   nn = ndiffs(m)
 
@@ -12,82 +11,13 @@ function compute_bengtsson(a0, a1, m)
   h = DefaultOrderedDict{NTuple{nhv,Int},valtype}(default)
   a1t = one(a1)
   setray!(a1t.v, v_matrix=jacobian(a1, HVARS))
-  #@show jacobian(a1t, ALL)
-  #@show jacobian(a0, HVARS)
-
-  a_cs = a0 ∘ a1t # cutord(a, 2) #cutord(a0 ∘ a1 ∘ a2, 2)
-  #cutord(a0, 2) ∘ a1t #a0 ∘ a1 #t
-#  cutord(a1, 2)
-#  cutord(a0 ∘ a1t, 2)#t #cutord(a1, 2) #t #cutord(a1, 2) #cutord(a0 ∘ a1, 2)
-#@show jacobian(a_cs, ALL)
-#error()
-  #@show jacobian(cutord(a1, 2), ALL)
- # error()
+  a_cs = a0 ∘ a1t
   c = c_map(a0)
   ci = ci_map(a0)
 
   r =  ci ∘ inv(a_cs) ∘ m ∘ a_cs ∘ c
-#=
-  # Gets the linear part but retains nonlinear parameter dependence
-  v = Ref{TI.numtype(eltype(r.v))}() # monomial value 
-  r_lin = zero(r)
-  tmp = zero(r.v[1])
-  ords = zeros(UInt8, nn)
 
-  for i in 1:nhv
-    TI.clear!(tmp)
-    ords .= 0
-    idx = TI.cycle!(r.v[i], 0, mono=ords, val=v)
-    while idx > -1
-      if sum(view(ords, 1:nhv)) <= 1
-        TI.setm!(tmp, v[], ords)
-      end
-      idx = TI.cycle!(r.v[i], idx, mono=ords, val=v)
-    end
-    TI.copy_tps!(r_lin.v[i], tmp)
-  end
-  =#
-#=
-  # for the coasting part need to remove quadratic orbital part:
-  if nv==5
-    TI.clear!(tmp)
-    ords .= 0
-    nt = nv
-    idx = TI.cycle!(r.v[nt], 0, mono=ords, val=v)
-    while idx > -1
-      if sum(view(ords, 1:nhv)) <= 2
-        TI.setm!(tmp, v[], ords)
-      end
-      idx = TI.cycle!(r.v[nt], idx, mono=ords, val=v)
-    end
-    TI.seti!(tmp, 1, nt)
-    TI.copy_tps!(r_lin.v[nt], tmp)
-  end
-  =#
-  #@show (checksymp(r_lin))
-  #@show jacobian(r, ALL)
-  #@show jacobian(r_lin, ALL)
-  #error()
-#@show norm(jacobian(r, ALL) - jacobian(r_lin, ALL))
-  #=
-  rlin = zero(r)
-  mono = zeros(UInt8, nv)
-  for i in 1:nv
-    for j in 1:nv
-      mono[j] = 1
-      TI.add!(rlin.v[i], rlin.v[i], r.v[i][[mono...,0]])
-      TI.add!(rlin.v[i], rlin.v[i], r.v[i][[mono...,1]])
-      mono[j] = 0
-      #ast_var_slice!(rlin.v[i], r.v[i], j, nv)
-    end
-  end
-  =#
-  #r0, r1, r2 = factorize(r)
-  #r = ci ∘ inv(a1) ∘ inv(a0) ∘ m ∘ a0 ∘ a1 ∘ c
-  #@show jacobian(r, ALL)
   R_inv = inv(jacobian(r, HVARS)) # R is diagonal matrix
-  #@show R_inv
-  #@show jacobian(inv(rlin), ALL)
   ri = one(r)
   setray!(ri.v, v_matrix=R_inv)
   # Remove the linear part:
